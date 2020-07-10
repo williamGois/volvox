ee82b6f9d3cb69743e75f0ad58e53215ece69faa
williamGois <32098239+williamGois@users.noreply.github.com>
2020-07-10 17:55:13 -0300

    Update Version20200710200953.php

--- src/Migrations/Version20200710200953.php
+++ src/Migrations/Version20200710200953.php
@@ -19,17 +19,26 @@ final class Version20200710200953 extends AbstractMigration
 
     public function up(Schema $schema) : void
     {
+        $pass = "\$argon2id\$v=19\$m=65536,t=4,p=1\$hYmgLclZjPjRrSxEcWGEFg\$wXtmwLWHFldH4/SsScYBBcEci1uB77KQix6TBpW+E8M";
+        $role = "[\"ROLE_USER\"]";
+
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL');
+        $this->addSql('INSERT INTO `user` (`id`, `email`, `roles`, `password`) VALUES ("1", "admin@admin.com", "[\"ROLE_USER\"]", "'.$pass.'");');
+
     }
+    
 
     public function down(Schema $schema) : void
     {
+        $pass = "\$argon2id\$v=19\$m=65536,t=4,p=1\$hYmgLclZjPjRrSxEcWGEFg\$wXtmwLWHFldH4/SsScYBBcEci1uB77KQix6TBpW+E8M";
+        $role = "[\"ROLE_USER\"]";
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`');
+        $this->addSql('INSERT INTO `user` (`id`, `email`, `roles`, `password`) VALUES (NULL, "admin@admin.com", "", "'.$pass.'");');
     }
 }
